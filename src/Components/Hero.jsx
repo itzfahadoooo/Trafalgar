7eb71e1a2099ca67a6b365a337a1b4379a68d3ca
itzfahadoooo <foyindolapo@gmail.com>
2025-04-12 23:20:47 +0100

    Initial Commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,11 +4,12 @@ import image from "../assets/trafalgar-header illustration 1.png";
 const Hero = ({ id }) => {
   return (
     <div id={id} className="w-full pt-24">
+      
       <div className="max-w-[1280px] justify-center mx-auto flex items-center py-6 px-8 md:px-16 md:justify-between flex-wrap gap-8">
         <div className="w-[445px] h-auto flex flex-col gap-[24px] items-center justify-center text-center">
           <h1 className="text-[#000] text-[48px] font-bold ">
             Virtual healthcare for you
-            
+
           </h1>
           <p className="text-[#7D7987] text-[21px] font-light">
             Trafalgar provides progressive, and affordable healthcare,
