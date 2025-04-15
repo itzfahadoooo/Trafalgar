0ba813dd11dc0f78553b20194cc0094f4503c4fb
itzfahadoooo <foyindolapo@gmail.com>
2025-04-15 21:45:19 +0100

    Initial Commit

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -60,6 +60,7 @@ const Services = ({ id }) => {
         </h1>
 
 
+
         <div className="w-[56px] h-[2px] rounded-[5px] bg-[#000]"></div>
 
         <p className="text-center text-[#7D7987] text-[18px] mb-12 ">
